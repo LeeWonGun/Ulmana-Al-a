d8e8e00a2a4a8a6a7c0013e8dc12f3e3a5b863b2
Admin <gktmdcjf1108@gmail.com>
2025-03-18 02:50:36 +0900

    0318 네비게이션바 제작

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
+    implementation("androidx.navigation:navigation-ui:2.7.6")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
